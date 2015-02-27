a136a72f8370968e65e0119e9ed19b92a7ee5bcc
kmruehl <kmruehl@sandia.gov>
2015-02-26 17:38:52 -0700

    more omae2015

--- PTO-Sim/ptoSimStartUp.m
+++
@@ -1,7 +0,0 @@
-% Add PTO-Sim files to MATLAB path
-ptoSimPath = 'Y:\MATLAB\PTO-Sim'; % Set this variable to the location of the PTO-Sim source directory. E.g. in Windows ptoSimPath = 'C:\Users\rso\PTO-Sim', in OSX/Linux ptoSimPath = '/home/rso/PTO-Sim'
-fprintf('Adding PTO-Sim to Path\n');
-addpath(ptoSimPath);
-folderToAdd = 'Source';
-addpath(genpath([ptoSimPath filesep folderToAdd]));
-clear ptoSimPath folderToAdd
